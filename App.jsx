3bc4055f6a5d8d410a056d0d986e7f6c5b026d34
YunShin <2younsin@gmail.com>
2022-02-09 21:17:30 +0900

    앱 개발환경구축 stackNavi까지

--- App.jsx
+++ App.jsx
@@ -1,46 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Alert, ScrollView } from "react-native";
-import PlusButton from "./components/PlusButton";
-import MinusButton from "./components/MinusButton";
+import { StyleSheet } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./navigations/StackNavigator";
 
 export default function App() {
-  const [state, setState] = useState(0);
-
-  function onChangeNumber(operation) {
-    if (operation == "plus") {
-      setState(state + 1);
-    } else {
-      setState(state - 1);
-    }
-  }
-  useEffect(() => {}, [state]);
-
   return (
-    <View style={styles.contianer}>
-      <Text style={styles.number}>카운터: {state}</Text>
-      <View style={styles.buttonBox}>
-        <PlusButton onChangeNumber={onChangeNumber} />
-        <MinusButton onChangeNumber={onChangeNumber} />
-      </View>
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  contianer: {
+  container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
     alignItems: "center",
-  },
-
-  number: {
-    fontSize: 30,
-  },
-
-  buttonBox: {
-    marginTop: 50,
-    flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
   },
 });
